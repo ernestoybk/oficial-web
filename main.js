f92d0ed26d49a7bfb47bf0902754d71455803fc3
ernestoybk <ernestoybk@gmail.com>
2024-04-16 21:58:28 -0500

    Cookies eliminados de html y css

--- main.js
+++ main.js
@@ -178,31 +178,3 @@ ScrollReveal().reveal('.skills-description, .service-description, .contact-card,
 ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', {delay:600, origin: 'bottom', interval: 200});
 ScrollReveal().reveal('footer .group', {delay:300, origin: 'top', interval: 200});
 
-//Coockies box
-let popUp = document.getElementById("cookiePopup");
-document.getElementById("acceptCookie").addEventListener("click", () => {
-    let d = new Date();
-    d.setMinutes(2 + d.getMinutes());
-    document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
-    popUp.classList.add("hide");
-    popUp.classList.remove("show");
-});
-
-const checkCookie = () => {
-    let input = document.cookie.split("=");
-    if (input[0] == "myCookieName") {
-        popUp.classList.add("hide");
-        popUp.classList.remove("show");
-    }
-    else{
-        popUp.classList.add("show");
-        popUp.classList.remove("hide");
-    }
-}
-
-window.onload = () => {
-    setTimeout(() => {
-        checkCookie();
-    }, 2000);
-};
-
