801ae57c50c31f5c72a5a04d3f97d3bffac39aae
Yvan Bazalar <33636785+ernestoybk@users.noreply.github.com>
2024-04-16 22:19:40 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,26 +78,23 @@ var swiper = new Swiper(".client-swiper", {
 });
 
 //Website dark/light theme
-const themeBtn = document.querySelector(".theme-btn");
+//const themeBtn = document.querySelector(".theme-btn");
 
-themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-theme");
-    themeBtn.classList.toggle("sun");
+//themeBtn.addEventListener("click", () => {
+//    document.body.classList.toggle("dark-theme");
+//    themeBtn.classList.toggle("sun");
 
-    localStorage.setItem("saved-theme", getCurrentTheme());
-    localStorage.setItem("saved-icon", getCurrentIcon());
-});
+//    localStorage.setItem("saved-theme", getCurrentTheme());
+//    localStorage.setItem("saved-icon", getCurrentIcon());
+//});
+//cconst getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";//cconst getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+//cconst savedTheme = localStorage.getItem("saved-theme");
+//const savedIcon = localStorage.getItem("saved-icon");
 
-const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
-const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
-
-const savedTheme = localStorage.getItem("saved-theme");
-const savedIcon = localStorage.getItem("saved-icon");
-
-if(savedTheme){
-    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
-    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"] ("sun");
-}
+//if(savedTheme){
+//    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+//    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"] ("sun");
+//}
 
 
 //Scroll to top button
